abaf2b8801de907fef5487076b98a6e7b45bb1bb
Juan Candia <jpcandia69@ucatolica.edu.co>
2021-01-09 00:25:25 -0500

    Se terminaron todos los moduos referentes a los informes de cada uno de los productos que administra la aplicación, tanto informes de registros, como informes de transacciones, y adicionalmente todos estos modulos fueron añadidos en la vista inicial luego de que un usuario realiza la autenticación en la aplicación

---
+++ modules/almacenamiento_stock_inventory/view.php
@@ -0,0 +1,62 @@
+
+<section class="content-header">
+  <h1>
+    <i class="fa fa-file-text-o icon-title"></i> Stock de Almacenamiento
+
+    <a class="btn btn-primary btn-social pull-right" href="modules/almacenamiento_stock_inventory/print.php" target="_blank">
+      <i class="fa fa-print"></i> Imprimir
+    </a>
+  </h1>
+
+</section>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="box box-primary">
+        <div class="box-body">
+        
+          <table id="dataTables1" class="table table-bordered table-striped table-hover">
+          
+            <thead>
+              <tr>
+                <th class="center">No.</th>
+                <th class="center">Codigo de almacenamiento</th>
+                <th class="center">Detalle</th>
+                <th class="center">Precio de compra</th>
+                <th class="center">Precio de venta</th>
+                <th class="center">Stock</th>
+              </tr>
+            </thead>
+          
+            <tbody>
+            <?php  
+            $no = 1;
+          
+            $query = mysqli_query($mysqli, "SELECT codigo_almacenamiento,tamaño,precio_compra,precio_venta,stock FROM almacenamiento ORDER BY tamaño ASC")
+                                            or die('Error: '.mysqli_error($mysqli));
+
+           
+            while ($data = mysqli_fetch_assoc($query)) { 
+              $precio_compra = format_rupiah($data['precio_compra']);
+              $precio_venta = format_rupiah($data['precio_venta']);
+             
+              echo "<tr>
+                      <td width='30' class='center'>$no</td>
+                      <td width='80' class='center'>$data[codigo_almacenamiento]</td>
+                      <td width='180'>$data[tamaño]</td>
+                      <td width='100' align='right'>$. $precio_compra</td>
+                      <td width='100' align='right'>$. $precio_venta</td>
+                      <td width='80' align='right'>$data[stock]</td>
+                    </tr>";
+              $no++;
+            }
+            ?>
+            </tbody>
+          </table>
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div><!--/.col -->
+  </div>   <!-- /.row -->
+</section><!-- /.content
--- modules/start/view.php
+++ modules/start/view.php
@@ -22,6 +22,9 @@
     </div>
    
     <!-- Small boxes (Stat box) -->
+
+    <!-- Modulos de camas y colchones -->
+
     <div class="row">
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
@@ -29,21 +32,21 @@
           <div class="inner">
             <?php  
           
-            $query = mysqli_query($mysqli, "SELECT COUNT(codigo) as numero FROM medicamentos")
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_cama) as numero FROM camas")
                                             or die('Error '.mysqli_error($mysqli));
 
            
             $data = mysqli_fetch_assoc($query);
             ?>
             <h3><?php echo $data['numero']; ?></h3>
-            <p>Datos de Muebles</p>
+            <p>Datos de Camas y colchones</p>
           </div>
           <div class="icon">
             <i class="fa fa-folder"></i>
           </div>
           <?php  
           if ($_SESSION['permisos_acceso']!='gerente') { ?>
-            <a href="?module=form_medicines&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+            <a href="?module=form_camas&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
           <?php
           } else { ?>
             <a class="small-box-footer"><i class="fa"></i></a>
@@ -59,7 +62,7 @@
           <div class="inner">
             <?php   
    
-            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_medicamentos")
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_camas")
                                             or die('Error '.mysqli_error($mysqli));
 
 
@@ -73,7 +76,7 @@
           </div>
           <?php  
           if ($_SESSION['permisos_acceso']!='gerente') { ?>
-            <a href="?module=form_medicines_transaction&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+            <a href="?module=form_camas_transaction&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
           <?php
           } else { ?>
             <a class="small-box-footer"><i class="fa"></i></a>
@@ -89,18 +92,18 @@
           <div class="inner">
             <?php  
   
-            $query = mysqli_query($mysqli, "SELECT COUNT(codigo) as numero FROM medicamentos")
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_cama) as numero FROM camas")
                                             or die('Error'.mysqli_error($mysqli));
 
             $data = mysqli_fetch_assoc($query);
             ?>
             <h3><?php echo $data['numero']; ?></h3>
-            <p>Stock Muebles</p>
+            <p>Stock Camas y colchones</p>
           </div>
           <div class="icon">
             <i class="fa fa-file-text-o"></i>
           </div>
-          <a href="?module=stock_inventory" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+          <a href="?module=camas_stock_inventory" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
         </div>
       </div><!-- ./col -->
 
@@ -110,19 +113,342 @@
           <div class="inner">
             <?php   
   
-            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_medicamentos")
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_camas")
                                             or die('Error: '.mysqli_error($mysqli));
 
             $data = mysqli_fetch_assoc($query);
             ?>
             <h3><?php echo $data['numero']; ?></h3>
-            <p>Registros de Muebles</p>
+            <p>Registros de Camas</p>
           </div>
           <div class="icon">
             <i class="fa fa-clone"></i>
           </div>
-          <a href="?module=stock_report" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+          <a href="?module=camas_stock_report" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+        
+      </div><!-- ./col -->
+    </div><!-- /.row -->  
+
+<!-- Modulos de Sofas -->
+
+    <div class="row">
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00c0ef;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+          
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_sofa) as numero FROM sofas")
+                                            or die('Error '.mysqli_error($mysqli));
+
+           
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Datos de Sofas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-folder"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_sofas&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
         </div>
       </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00a65a;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+   
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_sofas")
+                                            or die('Error '.mysqli_error($mysqli));
+
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>datos de entrada de inventario</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-sign-in"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_sofas_transaction&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#f39c12;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_sofa) as numero FROM sofas")
+                                            or die('Error'.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Stock Sofas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-file-text-o"></i>
+          </div>
+          <a href="?module=sofas_stock_inventory" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#dd4b39;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_sofas")
+                                            or die('Error: '.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Registros de Sofas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-clone"></i>
+          </div>
+          <a href="?module=sofas_stock_report" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+        
+      </div><!-- ./col -->
     </div><!-- /.row -->
+
+  <!-- Modulos de Almaceamiento -->
+
+  <div class="row">
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00c0ef;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+          
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_almacenamiento) as numero FROM almacenamiento")
+                                            or die('Error '.mysqli_error($mysqli));
+
+           
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Datos de Almacenamiento</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-folder"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_almacenamiento&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00a65a;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+   
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_almacenamiento")
+                                            or die('Error '.mysqli_error($mysqli));
+
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>datos de entrada de inventario</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-sign-in"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_almacenamiento_transaction&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#f39c12;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_almacenamiento) as numero FROM almacenamiento")
+                                            or die('Error'.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Stock Almacenamiento</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-file-text-o"></i>
+          </div>
+          <a href="?module=almacenamiento_stock_inventory" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#dd4b39;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_almacenamiento")
+                                            or die('Error: '.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Registros de Almacenamiento</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-clone"></i>
+          </div>
+          <a href="?module=almacenamiento_stock_report" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+        
+      </div><!-- ./col -->
+    </div><!-- /.row -->
+
+<!-- Modulos de Mesas -->
+
+<div class="row">
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00c0ef;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+          
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_mesa) as numero FROM mesas")
+                                            or die('Error '.mysqli_error($mysqli));
+
+           
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Datos de Mesas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-folder"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_mesas&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#00a65a;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+   
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_mesas")
+                                            or die('Error '.mysqli_error($mysqli));
+
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>datos de entrada de inventario</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-sign-in"></i>
+          </div>
+          <?php  
+          if ($_SESSION['permisos_acceso']!='gerente') { ?>
+            <a href="?module=form_mesas_transaction&form=add" class="small-box-footer" title="Agregar" data-toggle="tooltip"><i class="fa fa-plus"></i></a>
+          <?php
+          } else { ?>
+            <a class="small-box-footer"><i class="fa"></i></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#f39c12;color:#fff" class="small-box">
+          <div class="inner">
+            <?php  
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_mesa) as numero FROM mesas")
+                                            or die('Error'.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Stock Mesas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-file-text-o"></i>
+          </div>
+          <a href="?module=mesas_stock_inventory" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+      </div><!-- ./col -->
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div style="background-color:#dd4b39;color:#fff" class="small-box">
+          <div class="inner">
+            <?php   
+  
+            $query = mysqli_query($mysqli, "SELECT COUNT(codigo_transaccion) as numero FROM transaccion_mesas")
+                                            or die('Error: '.mysqli_error($mysqli));
+
+            $data = mysqli_fetch_assoc($query);
+            ?>
+            <h3><?php echo $data['numero']; ?></h3>
+            <p>Registros de Mesas</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-clone"></i>
+          </div>
+          <a href="?module=mesas_stock_report" class="small-box-footer" title="Imprimir" data-toggle="tooltip"><i class="fa fa-print"></i></a>
+        </div>
+        
+      </div><!-- ./col -->
+    </div><!-- /.row -->
+    
   </section><!-- /.content -->
